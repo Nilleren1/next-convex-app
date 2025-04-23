c4299a23f658e51fa59781bb880ba6a5db8c44f6
Nilleren1 <nicolai.hoegsbjerg@gmail.com>
2025-04-23 17:09:52 +0200

    darkmode with shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ConvexClientProvider } from "./ConvexClientProvider";
 import "./globals.css";
+import Navbar from "./components/Navbar";
+import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,11 +26,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        suppressHydrationWarning className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+    <html lang="en" suppressHydrationWarning>
+      <body 
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-       <ConvexClientProvider>{children}</ConvexClientProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar/>
+          
+          <ConvexClientProvider>{children}</ConvexClientProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
   const todos = useQuery(api.todos.getTodos);
 
   return (
+  
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex row-start-2 items-center sm:items-start gap-5">
         <form className="flex flex-col gap-2 items-center" onSubmit={(e) => {
@@ -18,7 +19,7 @@ export default function Home() {
           createTask({ text: text.trim() });
           setText('');
         }}>
-          <input className="border-white border rounded-md px-2 py-1" value={text} onChange={(e) => setText(e.target.value)} />
+          <input className="border border-black dark:border-white rounded-md px-2 py-1" value={text} onChange={(e) => setText(e.target.value)} />
           <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 border border-blue-500 hover:border-transparent rounded w-1/2">Create</button>
         </form>
         <div className="flex flex-col divide-y divide-gray-200">
