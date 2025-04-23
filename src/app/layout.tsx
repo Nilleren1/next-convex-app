097e0bc691cad711fc24a53a1ad6ac6da7a06322
Nilleren1 <nicolai.hoegsbjerg@gmail.com>
2025-04-23 21:34:30 +0200

    Refactor page components; remove unused button, dropdown, and menubar components
    - Refactored `page.tsx` to simplify the structure and remove test task management logic from convex.
    - Deleted unused `button.tsx`, `dropdown-menu.tsx`, and `menubar.tsx` components.
    - Added new implementations of button, dropdown menu, and menubar components in `src/app/components`.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body 
+      <body
+        suppressHydrationWarning 
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider
