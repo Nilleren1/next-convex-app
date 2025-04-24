e2e4f22fcb9ae02f253fbe6b50b48519fc8ce1e7
Nilleren1 <nicolai.hoegsbjerg@gmail.com>
2025-04-24 17:54:17 +0200

    init commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
 "use client";
+import React from 'react'
+import Image from "next/image";
+import containerImage from "../../public/pc-image.webp";
 
 export default function Home() {
 
@@ -6,13 +9,13 @@ export default function Home() {
     <div>
       <div className="grid grid-cols-6 sm:grid-rows-[2fr_1fr] gap-4 p-8 sm:h-[90vh] h-auto sm:mx-15 mx-5">
         {/* Row 1: Two large containers */}
-        <div className="page-container sm:col-span-3 col-span-6">Hello dear user</div>
-        <div className="page-container sm:col-span-3 col-span-6">Large Container 2</div>
+        <div className="page-container sm:col-span-3 col-span-6 p-8">Hello dear user</div>
+        <div className="page-container sm:col-span-3 col-span-6"><Image src={containerImage} priority sizes="(max-width: 640px) 80vw, 600vw" className="container-image" alt=''></Image></div>
 
         {/* Row 2: Three smaller containers */}
-        <div className="page-container sm:col-span-2 col-span-6">Small Container 1</div>
-        <div className="page-container sm:col-span-2 col-span-6">Small Container 2</div>
-        <div className="page-container sm:col-span-2 col-span-6">Small Container 3</div>
+        <div className="page-container sm:col-span-2 col-span-6 p-8">Small Container 1</div>
+        <div className="page-container sm:col-span-2 col-span-6 p-8">Small Container 2</div>
+        <div className="page-container sm:col-span-2 col-span-6 p-8">Small Container 3</div>
       </div>
     </div>
   );
