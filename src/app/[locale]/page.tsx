642c347a544c2bfba657a7d263267b7d59dfffe0
Nilleren1 <nicolai.hoegsbjerg@gmail.com>
2025-04-24 19:11:19 +0200

    Implement internationalization with next-intl; add routing and middleware support

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,15 +1,18 @@
 "use client";
 import React from 'react'
 import Image from "next/image";
-import containerImage from "../../public/pc-image.webp";
+import containerImage from "../../../public/pc-image.webp";
+import {useTranslations} from 'next-intl';
+// import {Link} from '@/i18n/navigation';
 
 export default function Home() {
+  const t = useTranslations('HomePage');
 
   return (
     <div>
       <div className="grid grid-cols-6 sm:grid-rows-[2fr_1fr] gap-4 p-8 sm:h-[90vh] h-auto sm:mx-15 mx-5">
         {/* Row 1: Two large containers */}
-        <div className="page-container sm:col-span-3 col-span-6 p-8">Hello dear user</div>
+        <div className="page-container sm:col-span-3 col-span-6 p-8">{t('title')}</div>
         <div className="page-container sm:col-span-3 col-span-6"><Image src={containerImage} priority sizes="(max-width: 640px) 80vw, 600vw" className="container-image" alt=''></Image></div>
 
         {/* Row 2: Three smaller containers */}
