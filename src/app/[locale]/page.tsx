98ab83b15c86ac2837787e221b7a516db66c3217
Nilleren1 <nicolai.hoegsbjerg@gmail.com>
2025-04-25 10:47:19 +0200

    More text and link

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -11,7 +11,16 @@ export default function Home() {
     <>
       <div className="grid grid-cols-6 sm:grid-rows-[2fr_1fr] gap-5 sm:h-[80vh] h-auto ">
         {/* Row 1: Two large containers */}
-        <div className="page-container sm:col-span-3 col-span-6 p-8">{t('title')}</div>
+        <div className="page-container sm:col-span-3 col-span-6 p-8"><h1 className="text-xl font-medium mb-2">{t('title')}</h1>
+        <div className='space-y-4'>
+          <p>{t('message1')}</p>
+          <p>{t('message2')}</p>
+          <p>{t('message3')}
+            <br />
+            <a className='text-blue-500 hover:text-blue-700 transition-colors duration-300' href="https://www.linkedin.com/in/nicolai-hogsbjerg">www.linkedin.com/in/nicolai-hogsbjerg</a>
+          </p>
+        </div>
+        </div>
         <div className="page-container sm:col-span-3 col-span-6"><Image src={containerImage} priority sizes="(max-width: 640px) 80vw, 600vw" className="container-image" alt=''></Image></div>
 
         {/* Row 2: Three smaller containers */}
