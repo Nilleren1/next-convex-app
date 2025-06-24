dab7e8a8fd3b2095fe4d229ed9947bed5386b8b2
Nilleren1 <nicolai.hoegsbjerg@gmail.com>
2025-06-24 19:20:57 +0200

    truncate text

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
  
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+    ],
+    dangerouslyAllowSVG: true,
+  },
+};
  
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -1,25 +1,54 @@
-"use client"
+"use client";
 
-import { useQuery } from "convex/react"
-import { api } from "../../../../convex/_generated/api"
+import Image from "next/image";
+import { useQuery, useMutation } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+
+import { Card } from "../../../components/ui/Card";
 
 export default function Page() {
-  const posts = useQuery(api.blog.getPosts)
+  const posts = useQuery(api.blog.getPosts);
+  const createPost = useMutation(api.blog.createPost);
+
+  const textLimit = 100;
+  const truncateText = (text) => {
+    if (text.length > textLimit) {
+      return text.slice(0, textLimit) + "...";
+    }
+    return text;
+  };
 
   return (
     <>
-      <h1>Blog</h1>
-      <p>Here you can find the latest posts, relevant about me and my career.</p>
-      <ul>
+      <div className="mb-10 text-center ">
+        <h1 className="text-3xl font-semibold">Blog</h1>
+        <p>
+          Here you can find the latest posts, relevant about me and my career.
+        </p>
+      </div>
+      {/* <button onClick={addDummyPost}>Add Dummy Post</button> */}
+      <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
         {posts?.map((post) => (
-          <li key={post._id}>
-            <h2>{post.header}</h2>
-            <img src={post.image} alt={post.header} style={{ maxWidth: 200 }} />
-            <p>{post.text}</p>
-            <small>{new Date(post.createdAt).toLocaleString()}</small>
+          <li key={post._id} className="mb-8">
+            <Card className="mb-0 h-full flex flex-col shadow-lg hover:shadow-xl transition-shadow duration-300 max-w-xs mx-auto">
+              <Image
+                src={post.image}
+                alt={post.header}
+                width={0}
+                height={0}
+                className="w-full h-auto object-cover"
+              />
+              <div className="p-4">
+                <h2 className="text-2xl font-semibold mb-2">{post.header}</h2>
+                <p className="mb-2">{truncateText(post.text)}</p>
+                <small>
+                  {new Date(post.createdAt).toLocaleDateString("da-DK")}
+                </small>
+              </div>
+            </Card>
           </li>
         ))}
       </ul>
     </>
-  )
+  );
 }
